96a8f28a19b1c2c54d2ed2614b8577655549ba81
Christopher DeMarco <cdemarco@gmail.com>
2018-08-21 22:51:21 -0400

    WIP

--- backupDropbox.sh
+++ backupDropbox.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 DBD=~/cmd/BAK/Dropbox
-[ -d $DBD ] || mkdir -p $DBD
-rdiff-backup --print-statistics ~/Dropbox $DBD
+[ -e $DBD ] || mkdir -p $DBD
+rdiff-backup --print-statistics ~/cmd/Dropbox\ \(Personal\) $DBD
