bada390b1808601bf09d8f6624429303b1675428
Christopher DeMarco <christopher.demarco@nielsen.com>
2020-11-06 16:39:56 -0500

    Make it actually calculate correctly

--- journal.py
+++ journal.py
@@ -4,21 +4,27 @@ from datetime import datetime
 import sys
 
 
-page_on = int(input("Current page? "))
+if len(sys.argv) == 1:
+    page_on = int(input("Current page? "))
+else:
+    page_on = int(sys.argv[1])
 day_of_year = datetime.now().timetuple().tm_yday
-#day_of_year = int(input("Day of year? "))
 
 book_len = 250
-pages_remaining = 365 - book_len
-pages_per_month = int(2 + (30 * (5 / 7)))    # 2 for month spread + only write on weekdays
+pages_remaining = book_len - page_on
 
 days_remaining = 365 - day_of_year
+writing_days_remaining = int(days_remaining * 5 / 7) # only write on weekdays
 months_remaining = int(days_remaining / 30)
-expected_pages_used = int(months_remaining * pages_per_month)
 
+# including monthly spreads
+expected_pages_used = 2 * months_remaining + writing_days_remaining
 
+expected_end_page = page_on + expected_pages_used
+expected_unwritten_pages = book_len - expected_end_page
+expected_unwritten_percent = expected_unwritten_pages / book_len * 100
 
-print(f"{days_remaining} days ({months_remaining} months) to fill {book_len - page_on} pages.")
-print(f"Averaging one page per day, expect {expected_pages_used} pages on daily logs, leaving {pages_remaining - expected_pages_used} free.")
-print(f"At this rate, the year will finish on page {book_len - expected_pages_used}.")
+print(f"{days_remaining} days  remain to fill {pages_remaining} pages.")
+print(f"Averaging one page per weekday, expect {expected_pages_used} pages to be written.")
+print(f"Thus the year will finish on page {expected_end_page}, leaving {expected_unwritten_pages} ({expected_unwritten_percent}%) unwritten.")
 
